d9688647469c6d7d52749919b500a803403cea6f
SkBakshu <bakshua0@gmail.com>
2025-07-17 22:47:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(
   '/uploads',
   express.static(path.join(__dirname, 'uploads'), {
     setHeaders: (res, _path) => {
-      res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+      res.set('Access-Control-Allow-Origin', 'https://resumexpress-frontends.onrender.com');
     },
   })
 );
